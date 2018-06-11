db4dd181291178546bb2db8468c0626556a7585a
香饽饽zizizi <863461783@qq.com>
2018-06-11 16:26:02 +0800

    hehe

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const projectHandler = (event, action) => {
 http.createServer((req, res) => {
 	handler(req, res, err => {
 		res.statusCode = 404
-		res.end('自动化部署之旅！')
+		res.end('hehe')
 	})
 }).listen(port, () => {
   console.log(new Date(), `Deploy server Run！port at ${port}`)
