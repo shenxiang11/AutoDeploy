6dd0582cfe77b3f9b9a718133895c1b2783b2772
香饽饽zizizi <863461783@qq.com>
2018-06-11 15:52:39 +0800

    my custom

--- projects/autodeploy.sh
+++ projects/autodeploy.sh
@@ -1,14 +1,11 @@
 WEB_PATH='/root/deploy'
-WEB_USER='root'
-WEB_USERGROUP='root'
  
 echo "Start deployment deploy"
 cd $WEB_PATH
 echo "pulling source code..."
-# git reset --hard origin/master
-# git clean -f
+
 git pull
 git checkout master
-echo "changing permissions..."
-chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
+
+pm2 restart ./server.js --name 'autodeploy'
 echo "Finished."
